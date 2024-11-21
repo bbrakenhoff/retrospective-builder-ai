0204bbd15b13363002579a796dcc8c5bab3f7987
KoetjesreepMacBook <koetjesreepmacbook@Bijoyas-MacBook-Air.local>
2024-11-21 21:14:15 +0100

    Add switch between light and dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [
+    RouterOutlet, 
+    SidebarComponent,
+    CommonModule
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html'
 })
 export class SidebarComponent {
-  siteName = 'Your Site Name';
+ public readonly siteName = 'Your Site Name';
+ public readonly isDarkMode = signal(false);
+
+  public toggleTheme() {
+    this.isDarkMode.update(dark => !dark);
+  }
 } 
