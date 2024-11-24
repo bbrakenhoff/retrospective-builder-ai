d266af91a9ee120d8176b460213e84bd66e19f5f
Bijoya Brakenhoff <bbrakenhoff@users.noreply.github.com>
2024-11-24 20:12:01 +0100

    Add empty component for a dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { RetrospectiveElementsComponent } from './components/retrospective-elements/retrospective-elements.component';
 import { RetrospectivesComponent } from './components/retrospectives/retrospectives.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'retrospectives',
+    redirectTo: 'dashboard',
     pathMatch: 'full',
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
   },
   {
     path: 'retrospective-elements',
