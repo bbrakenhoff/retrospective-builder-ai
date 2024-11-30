51928c0763108994991195c3b6e225ad3e8a81f3
Bijoya Brakenhoff <bbrakenhoff@users.noreply.github.com>
2024-11-30 22:52:19 +0100

    Move the reload force / empty check to the reload method

--- src/app/core/services/retrospective-element.service.ts
+++ src/app/core/services/retrospective-element.service.ts
@@ -15,22 +15,22 @@ export class RetrospectiveElementService {
   constructor(private notionService: NotionService) {}
 
   all$(force = false): Observable<RetrospectiveElement[]> {
-    if (force || this.cache$$.value.length === 0) {
-      this.reload();
-    }
+    this.reload(force);
     return this.cache$$.asObservable().pipe(share());
   }
 
-  reload(): void {
-    this.isLoading$$.next(true);
-    this.notionService
-      .getRetrospectiveElements$()
-      .pipe(take(1), share())
-      .subscribe({
-        next: elements => {
-          this.cache$$.next(elements);
-          this.isLoading$$.next(false);
-        },
-      });
+  reload(force = false): void {
+    if (force || this.cache$$.value.length === 0) {
+      this.isLoading$$.next(true);
+      this.notionService
+        .getRetrospectiveElements$()
+        .pipe(take(1), share())
+        .subscribe({
+          next: elements => {
+            this.cache$$.next(elements);
+            this.isLoading$$.next(false);
+          },
+        });
+    }
   }
 }
